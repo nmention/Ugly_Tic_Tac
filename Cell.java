a29dc1fd272ab708f42e52bae6bd57d00e8461ff
nmention <noe.mention@protonmail.com>
2023-01-27 13:04:18 +0100

    start of Vue

--- Cell.java
+++ Cell.java
@@ -8,6 +8,14 @@ public class Cell {
     static ImageIcon img;
 
 
+    public boolean isMarked() {
+        return marked;
+    }
+
+    public void setMarked(boolean marked) {
+        this.marked = marked;
+    }
+
     public Cell(){
         marked = false;
     }
--- Vue.java
+++ Vue.java
@@ -1,6 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Vue extends JFrame {
+    JPanel jpanel;
+    Grid grid;
 
+
+    public Vue(Grid grid){
+        super("Tic-tac-toe");
+        this.grid = grid;
+        jpanel = new JPanel();
+        jpanel.setLayout(new GridLayout(grid.size,grid.size));
+        for (int i = 0; i < grid.size * grid.size ; i++) {
+            JButton b = new JButton();
+            b.setBackground(Color.BLUE);
+            jpanel.add(b);
+        }
+
+
+
+        setVisible(true);
+        setSize(400,600);
+        setResizable(true);
+
+        this.setContentPane(jpanel);
+    }
+
+    public static void main(String[] args) {
+        Vue vue = new Vue(new Grid(3));
+    }
 
 }
